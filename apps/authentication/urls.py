eeadaa9bd407373de06dd9232212fed98756d46d
Bayane TOUKI <toukibayane@gmail.com>
2026-04-30 16:19:55 +0100

    Remove unused import from urls.py

--- apps/authentication/urls.py
+++ apps/authentication/urls.py
@@ -1,4 +1,3 @@
-from django.http import HttpResponse
 from django.urls import path
 from rest_framework_simplejwt.views import TokenRefreshView
 from .views import RegisterView, LoginView, LogoutView, ProfileView, ChangePasswordView
